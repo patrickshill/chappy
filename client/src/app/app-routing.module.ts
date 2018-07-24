04037bac91cc22342bc8263c1dd9741b63777e62
Patrick Hill <patrick.hill8991@gmail.com>
2018-07-23 23:11:36 -0700

    styling stuff

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SiteComponent } from './site/site.component';
+import { ChappComponent } from './chapp/chapp.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
 
 const routes: Routes = [
-  { path:'', redirectTo: '/', pathMatch: 'full'}
+  { path:"", redirectTo: '/home/splash', pathMatch: 'full'},
+  { path:"home", component: SiteComponent, children: [
+    { path: "splash", component: SplashPageComponent },
+    { path: "login", component: LoginComponent },
+    { path: "registration", component: RegistrationComponent }
+  ]},
+  { path:"chapp", component: ChappComponent }
 ];
 
 @NgModule({
