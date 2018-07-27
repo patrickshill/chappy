1a239298175a29609876773f2072420ee77699bd
James-MacBook-Pro <jrfoley13866@yahoo.com>
2018-07-27 04:29:34 -0700

    fixed dependency bugs

--- client/src/app/channel-users/channel-users.component.ts
+++ client/src/app/channel-users/channel-users.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { FormGroup} from '@angular/forms';
-import { log } from 'util';
-import { log } from 'util';
 import { LocalStorageService } from 'ngx-webstorage';
 
 @Component({
@@ -23,11 +21,11 @@ export class ChannelUsersComponent implements OnInit, AfterViewInit {
     this.user = {
       name: ''
     }
-    this.updateCurrentUser();
+    // this.updateCurrentUser();
   }
 
   ngAfterViewInit(){
-    this.getFriends();
+    // this.getFriends();
   }
 
 
@@ -35,12 +33,12 @@ export class ChannelUsersComponent implements OnInit, AfterViewInit {
     let users = this._httpService.getUserByName(this.user);
     users.subscribe(data => {
       if (data == null) {
-        this.errors = 'This user does not exist.';
+        this.errors = 'This user does not exist.'
         console.log(this.errors);
       }
       else if (data['username'] == this.currentUser.username){
         this.errors = 'You cannot add yourself!'
-        console.log(this.errors);
+        console.log(this.errors)
       }
       else {
         for (let x of this.currentUser.friendsList){
@@ -49,23 +47,25 @@ export class ChannelUsersComponent implements OnInit, AfterViewInit {
             console.log(this.errors);
             return this;
           }
-        }
-        this.errors = null;
-        let new_friend = {
-          id: this.currentUser.id
-          F_id: data['_id']
-        }
-        let friend = this._httpService.updateUser(new_friend);
-        friend.subscribe(data => {
-          console.log(data);
-          this.updateCurrentUser()
-        })
+          else {
+            this.errors = null;
+            let new_friend = {
+              id: this.currentUser.id,
+              F_id: data['_id']
+            };
+            let friend = this._httpService.updateUser(new_friend);
+            friend.subscribe(data => {
+              console.log(data);
+              this.updateCurrentUser()
+            })
+          }
+        };
+        };
       }
-    });
-  }
-
+    )};
+  
   updateCurrentUser(){
-    let update = this._httpService.getOneUser();
+    let update = this._httpService.getOneUser(this.currentUser.id);
     update.subscribe(data => {
       this.currentUser =  {
         id: data["_id"],
@@ -76,33 +76,31 @@ export class ChannelUsersComponent implements OnInit, AfterViewInit {
         channels: data["channels"],
         dm_channels: data["dm_channels"],
         friendsList: data["friendsList"]
-        this.localStorage.store('user', currentUser);
-        this.getFriends();
-      });
+      };
     });
-
-  
-  getFriends(){
-    this.allFriends = [];
-    if (this.currentUser.friendsList.length > 0){
-      for (var x of this.currentUser.friendsList){
-        let friends = this._httpService.getOneUser(x);
-        friends.subscribe(data => {
-          this.allFriends.push({
-            id: data['_id'],
-            username: data["username"],
-            email: data["email"],
-            avatar: data["avatar"],
-            status: data["status"],
-          });
-        });
-      }
-      console.log(this.allFriends);
-    }
-    else {
-      console.log('you have no friends');
-    }
   }
+
+  // getFriends(){
+  //   this.allFriends = [];
+  //   if (this.currentUser.friendsList.length > 0){
+  //     for (var x of this.currentUser.friendsList){
+  //       let friends = this._httpService.getOneUser(x);
+  //       friends.subscribe(data => {
+  //         this.allFriends.push({
+  //           id: data['_id'],
+  //           username: data["username"],
+  //           email: data["email"],
+  //           avatar: data["avatar"],
+  //           status: data["status"],
+  //         });
+  //       });
+  //     }
+  //     console.log(this.allFriends);
+  //   }
+  //   else {
+  //     console.log('you have no friends');
+  //   }
+  // }
 
   showModal() {
     this.show = true;
